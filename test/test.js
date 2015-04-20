e28cc62245ef9bc0a3e590a55d5ecf7c5ebc25ba
Assinnata <matteo.assinnata@cryptodeer.com>
2015-04-20 18:21:53 +0200

    foobar test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var c2j = require("../index.js");
 
 c2j("foobar", function(success, jconf){
+	console.log(success)
+	console.log(jconf)
 	if(success && jconf["a"] == 33 && jconf.foobar == "raboof" )
 		console.log("test success");
 	else
